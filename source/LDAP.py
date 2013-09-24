6d8e9578e15c6354d1a191ee18525e04e8b622ed
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-09-24 16:07:10 -0300

    add license

--- source/LDAP.py
+++ source/LDAP.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+# ldapsync v2013-alpha
+# Copyright (c) 2013 - Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 import ldap
 
@@ -34,7 +51,6 @@ class LDAP(object):
                     print "ERROR: result type not implemented. %s" % result_type
             return ldap_result
         except ldap.LDAPError, e:
-            #self.parse_exception("LDAPError", e)
             if e[0]["desc"] == "Size limit exceeded":
                 return ldap_result
 
