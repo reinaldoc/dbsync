cacba70be35ce2e964b8fb53d93bebb15a418e23
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-11-22 12:13:32 -0300

    new method to get data from source database by configurated query

--- source/dao/OracleDAO.py
+++ source/dao/OracleDAO.py
@@ -10,6 +10,10 @@ class OracleDAO(object):
     self.conn = cx_Oracle.connect("%s/%s@%s" % (self.c.config.get(db_section, "username") , self.c.config.get(db_section, "password") , self.c.config.get(db_section, "uri")))
     self.cursor = self.conn.cursor()
 
+  def execute(self):
+    self.cursor.execute(self.c.config.get(self.sync_section, "from query"))
+    return self.cursor.fetchall()
+
   def test(self):
     self.cursor.execute(self.c.config.get(self.sync_section, "from query"))
     result = self.cursor.fetchone()
