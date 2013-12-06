fe4820df9586c5d14b0c7b338a8ec2d5b9c7225b
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-06 13:15:13 -0300

    Fix replace with UTF-8 caracters

--- source/util/Strings.py
+++ source/util/Strings.py
@@ -5,5 +5,5 @@ class Strings(object):
     def replaceList(string, list):
         for count in range(0,len(list)):
             if list[count] is not None:
-                string = string.replace("%%%s" % count, list[count])
+                string = string.replace("%%%s" % count, list[count].decode("utf-8"))
         return string
