88bf5656b4a0f6aeb3ca16d15841da5a1ffb07c0
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-11 17:34:13 -0300

    implement validation for required attributes for sync sections

--- source/dao/ConfigDAO.py
+++ source/dao/ConfigDAO.py
@@ -18,15 +18,43 @@ class ConfigDAO(object):
 		ConfigDAO._construct = True
 
 		# Initialize
+		self.sync_sections = None
 		self.config = ConfigParser.ConfigParser()
 		self.config.read("dbsync.conf")
+		self.__validate_config()
 
-	def getSyncSections(self):
-		result = []
+	def __validate_config(self):
+		self.__validate_required_attributes()
+
+	def __validate_required_attributes(self):
+		for sync_section in self.get_sync_sections():
+			try:
+				self.config.get(sync_section, "from")
+				self.config.get(sync_section, "from query")
+				self.config.get(sync_section, "to")
+				self.config.get(sync_section, "to update template")
+			except ConfigParser.NoOptionError, e:
+				print("Attribute '%s' is mandatory for synchronization '%s'. Aborting..." % (e[0], e[1]))
+				exit()
+
+	def get_sync_sections(self):
+		if self.sync_sections is not None:
+			return self.sync_sections
+
+		self.sync_sections = []
 		for section in self.config.sections():
 			try:
 				if self.config.get(section, "type") == "sync":
-					result.append(section)
-			except Exception, e:
+					self.sync_sections.append(section)
+			except ConfigParser.NoOptionError, e:
 				pass
-		return result
+		return self.sync_sections
+
+	def get(self, section, key):
+		""" get value by section/attribue from a ConfigParser object. Returns
+		None if section or attribute does not exist"""
+		try:
+			return self.config.get(section, key)
+		except ConfigParser.NoOptionError, e:
+			return None
+
