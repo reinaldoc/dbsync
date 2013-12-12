224a887d9a872809b19cdf2939c54d3ee7e2f979
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-12 18:52:10 -0300

    separate business code to OracleBC and persistence code to OracleDAO

--- source/controller/OracleBC.py
+++ source/controller/OracleBC.py
@@ -1,15 +1,18 @@
 
+from dao.ConfigDAO import ConfigDAO
 from dao.OracleDAO import OracleDAO
 
 class OracleBC:
 
 	def __init__(self, db_section, sync_section):
+		self.c = ConfigDAO()
 		self.db_section = db_section
 		self.sync_section = sync_section
-		self.conn = OracleDAO(db_section, sync_section)
+		self.conn = OracleDAO(self.c.config.get(db_section, "uri"), self.c.config.get(db_section, "username"), self.c.config.get(db_section, "password"))
+		self.conn.execute_results_blob_as_bytes = True
 
 	def load(self):
-		return []
+		return self.conn.execute(self.c.config.get(self.sync_section, "from query"))
 
 	def sync(self, sync_section, row):
 		pass
--- source/dao/OracleDAO.py
+++ source/dao/OracleDAO.py
@@ -3,59 +3,55 @@ import sys, os
 os.environ["NLS_LANG"] = "AMERICAN_AMERICA.WE8MSWIN1252"
 
 import cx_Oracle
-from ConfigDAO import ConfigDAO
 
 class OracleDAO(object):
 
-	def __init__(self, db_section, sync_section):
-		self.db_section = db_section
-		self.sync_section = sync_section
-		self.c = ConfigDAO()
+	def __init__(self, uri, username, password):
+		self.execute_results_blob_as_bytes = False
 		try:
-			self.conn = cx_Oracle.connect("%s/%s@%s" % (self.c.config.get(db_section, "username") , self.c.config.get(db_section, "password") , self.c.config.get(db_section, "uri")))
+			self.conn = cx_Oracle.connect("%s/%s@%s" % (username, password, uri))
+			self.cursor = self.conn.cursor()
 		except cx_Oracle.DatabaseError, e:
 			print(e[0].message.strip())
-			print("Unable to connect to '%s'. Aborting..." % db_section)
+			print("Unable to connect to '%s'. Aborting..." % uri)
 			sys.exit()
-		self.cursor = self.conn.cursor()
-
-	def execute(self):
-		try:
-			self.cursor.execute(self.c.config.get(self.sync_section, "from query"))
-			return self.convert(self.cursor.fetchall())
-		except cx_Oracle.DatabaseError, e:
-			print(e[0].message.strip())
-			print("Malformed query '%s' in setcion '%s'" % (self.c.config.get(self.sync_section, "from query"), self.sync_section))
-			sys.exit()
-
-	def test(self):
-		if not self.c.get("General", "stage") == "Test":
-			return
-
-		print "\nRunning test to '%s'" % self.db_section
-		self.cursor.execute(self.c.config.get(self.sync_section, "from query"))
-		result = self.cursor.fetchone()
-
-		if result == None:
-			print("Nenhum Resultado")
-		else:
-			while result:
-				print result
-				result = self.cursor.fetchone()
 
 	def __del__(self):
 		try:
 			self.cursor.close()
 			self.conn.close()
-		except Exception, e:
+		except:
 			pass
 
+	def execute(self, query):
+		try:
+			self.cursor.execute(query)
+			return self.convert(self.cursor.fetchall())
+		except cx_Oracle.DatabaseError, e:
+			print(e[0].message.strip())
+			print("Malformed query '%s'" % query)
+			sys.exit()
+
 	def convert(self, ilist):
-		if not self.c.get(self.sync_section, "from field type"):
+
+		if not self.execute_results_blob_as_bytes:
 			return ilist
-		for i in range(0, len(ilist)):
-			item = list(ilist[i])
-			print type(item[2])
-			item[2] = item[2].read()
-			ilist[i] = item
+
+		# find blob cols from first row
+		blob_cols = []
+		if len(ilist) > 0:
+			first_row = ilist[0]
+			for i in range(0,len(first_row)):
+				if isinstance(first_row[i], cx_Oracle.LOB):
+					blob_cols.append(i)
+					
+		# replace cx_Oracle.LOB object for bytes
+		if blob_cols:
+			for i in range(0, len(ilist)):
+				row = list(ilist[i])
+				for blob_col in blob_cols:
+					if row[blob_col]:
+						row[blob_col] = row[blob_col].read()
+				ilist[i] = row
+
 		return ilist
