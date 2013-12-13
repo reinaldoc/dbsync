05a3af45a2d18e94cc4361e2a28b588b738908d7
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-13 18:04:20 -0300

    add ResizeImageKeepingHeightRatio converter

---
+++ source/controller/convert/ResizeImageKeepingHeightRatio.py
@@ -0,0 +1,22 @@
+
+from dao.ImageDAO import ImageDAO
+
+class ResizeImageKeepingHeightRatio(object):
+
+	def __init__(self, data, args=()):
+		self.value = None
+		if not args or type(args[0]) != type(0):
+			print("WARN: invalid parameters for ResizeImageKeepingHeightRatio: %s" % args)
+			return
+
+		width = args[0]
+		try:
+			output_format = args[1]
+		except IndexError:
+			output_format = None
+
+		if data:
+			self.value = ImageDAO.resize_string_keeping_height_ratio(data, args[0], output_format=output_format)
+
+	def get_value(self):
+		return self.value
