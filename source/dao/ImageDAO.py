ce391323f0f28661809fa29f5a81356daa175831
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-13 18:04:48 -0300

    add ImageDAO to image manipulation

---
+++ source/dao/ImageDAO.py
@@ -0,0 +1,23 @@
+
+import Image, ImageOps
+from cStringIO import StringIO
+
+class ImageDAO():
+
+	@staticmethod
+	def resize_image_keeping_height_ratio(image, width, resizemode=Image.ANTIALIAS):
+		wpercent = (width / float(image.size[0]))
+		hsize = int((float(image.size[1]) * float(wpercent)))
+		return image.resize((width, hsize), resizemode)
+
+	@staticmethod
+	def resize_string_keeping_height_ratio(string, width, resizemode=Image.ANTIALIAS, output_format=None):
+
+		image = Image.open(StringIO(string))
+		if output_format is None:
+			output_format = image.format
+
+		image = ImageDAO.resize_image_keeping_height_ratio(image, width)
+		result = StringIO()
+		image.save(result, format=output_format)
+		return result.getvalue()
