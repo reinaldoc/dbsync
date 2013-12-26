a7aa86e41f58cfb741899d911328cf8513cd77c4
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-12-26 13:50:09 -0300

    validate string

--- source/util/Strings.py
+++ source/util/Strings.py
@@ -5,6 +5,12 @@ Strings - a class to string related code
 class Strings(object):
 
 	@staticmethod
+	def is_string(string):
+		if type(string) == type("str"):
+			return True
+		return False
+
+	@staticmethod
 	def replace_from_array(string, array=[], from_encoding="utf-8", to_encoding="utf-8"):
 		"""
 		Receives a string template like 'My name is %0 and I am %1 years old' and
@@ -25,23 +31,24 @@ class Strings(object):
 		Receive a string and returns a copy of that with all words capitalized,
 		except the prepositions.
 		"""
-		prepositions = (" Da ", " Das ", " Do ", " Dos ",
-                                " De ", " Du ", " E ")
-		
-		string = string.title()
+		if not Strings.is_string(string):
+			return string
 
+		prepositions = (" Da ", " Das ", " Do ", " Dos ", " De ", " Du ", " E ")
+		string = string.title()
 		for prep in prepositions:
 			string = string.replace(prep, prep.lower())
+
 		return string
 
 	@staticmethod
-	def replace_string(string, actual, to):
+	def replace_string(string, s_from, s_to):
 		"""
 		Replace strings, receiving the "actual" string and replacing it as "to" string.
 		Also, converts any entry type to string.
 		"""
 		try:
-			string = string.replace(actual, to)
+			string = string.replace(s_from, s_to)
 		except Exception as erro:
 			print(erro)
 		
